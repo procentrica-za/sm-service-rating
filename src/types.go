dcd2e895653968526f30901f88b1a8529a79d384
hsgilliland <jo-ash@procentrica.co.za>
2020-03-16 17:44:25 +0200

    added function to start rating

--- src/types.go
+++ src/types.go
@@ -3,53 +3,18 @@ package main
 import "github.com/gorilla/mux"
 
 //create structs for JSON objects recieved and responses
-type StartChat struct {
+type StartRating struct {
+	AdvertisementID string `json:"advertisementid"`
 	SellerID        string `json:"sellerid"`
 	BuyerID         string `json:"buyerid"`
+	BuyerRating     string `json:"buyerating"`
+	BuyerComments   string `json:"buyercomments"`
+}
+
+type StartRatingResult struct {
+	BuyerRated      bool   `json:"buyerrated"`
 	AdvertisementID string `json:"advertisementid"`
-}
-
-type StartChatResult struct {
-	ChatPosted bool   `json:"chatposted"`
-	ChatID     string `json:"chatid"`
-	Message    string `json:"message"`
-}
-
-type ChatID struct {
-	ChatID string `json:"id"`
-}
-
-type DeleteChatResult struct {
-	ChatDeleted bool   `json:"chatposted"`
-	Message     string `json:"message"`
-}
-
-type GetActiveChatResult struct {
-	ChatID      string `json:"chatid"`
-	UserName    string `json:"username"`
-	Message     string `json:"message"`
-	MessageDate string `json:"messagedate"`
-}
-
-type ActiveChatList struct {
-	ActiveChats []GetActiveChatResult `json:"activechats"`
-}
-
-type GetMessageResult struct {
-	MessageID   string `json:"messageid"`
-	UserName    string `json:"username"`
-	Message     string `json:"message"`
-	MessageDate string `json:"messagedate"`
-}
-
-type MessageList struct {
-	Messages []GetMessageResult `json:"messages"`
-}
-
-type SendMessage struct {
-	ChatID   string `json:"chatid"`
-	AuthorID string `json:"authorid"`
-	Message  string `json:"message"`
+	Message         string `json:"message"`
 }
 
 //touter service struct
